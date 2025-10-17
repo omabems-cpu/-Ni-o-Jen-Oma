c131c0323d6ce92024c6a11e144f2f0f7808f54c
omabems-cpu <omabems@gmail.com>
2025-10-17 13:01:46 +0800

    Create OMA 17.py

---
+++ OMA 17.py
@@ -0,0 +1,20 @@
+import turtle
+import time
+
+t = turtle.Turtle()
+t.color("red")
+t.pensize(3)
+t.speed(2)
+
+t.begin_fill()
+t.left(140)
+t.forward(180)
+t.circle(-90, 200)
+t.left(120)
+t.circle(-90, 200)
+t.forward(180)
+t.end_fill()
+
+t.hideturtle()
+time.sleep(3)
+    
